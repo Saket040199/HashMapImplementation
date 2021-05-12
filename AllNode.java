7114923f5ec7f0cb1050f14f8cb21efb02753600
saket <saketj420@gmail.com>
2021-05-12 10:34:06 +0530

    Adding hashmap files

---
+++ AllNode.java
@@ -0,0 +1,15 @@
+package com.practice.hashmap;
+
+public interface AllNode<K> {
+ 
+	
+
+	    public K getKey();
+
+	    public void setKey(K key);
+
+	    public AllNode getNext();
+
+	    public void setNext(AllNode next);
+
+}
