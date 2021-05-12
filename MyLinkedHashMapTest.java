1800150d87e5a21027181dc3b03eabfb257e872b
Saket040199 <82606566+Saket040199@users.noreply.github.com>
2021-05-12 11:25:47 +0530

    Adding LinkedhashmapTest file

---
+++ MyLinkedHashMapTest.java
@@ -0,0 +1,47 @@
+package com.practice.hashmap;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MyLinkedHashMapTest {
+
+    @Test
+    public void givenASentence_WhenWordsAreAddedToList_ShouldReturnWordFrequency() {
+        MyLinkedHashMap<String, Integer> myLinkedHashMap = new MyLinkedHashMap<>();
+        String paragraph = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+        String[] words = paragraph.toLowerCase().split(" ");
+
+        for (String word : words) {
+            Integer value = myLinkedHashMap.get(word);
+            if (value == null)
+                value = 1;
+            else
+                value++;
+            myLinkedHashMap.add(word, value);
+        }
+        System.out.println(myLinkedHashMap.toString());
+        Assert.assertEquals(3, (int) myLinkedHashMap.get("paranoid"));
+    }
+
+    @Test
+    public void givenASentence_WhenWordIsRemoved_ShouldReturnNullFrequency() {
+        MyLinkedHashMap<String, Integer> myLinkedHashMap = new MyLinkedHashMap<>();
+        String paragraph = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+        String[] words = paragraph.toLowerCase().split(" ");
+
+        for (String word : words) {
+            Integer value = myLinkedHashMap.get(word);
+            if (value == null)
+                value = 1;
+            else
+                value++;
+            myLinkedHashMap.add(word, value);
+        }
+        System.out.println(myLinkedHashMap.toString());
+        System.out.println("Frequency before deletion : " + myLinkedHashMap.get("avoidable"));
+        myLinkedHashMap.deleteKey("avoidable");
+        System.out.println(myLinkedHashMap.toString());
+        System.out.println("Frequency after deletion : " + myLinkedHashMap.get("avoidable"));
+        Assert.assertEquals(null, myLinkedHashMap.get("avoidable"));
+    }
+}
